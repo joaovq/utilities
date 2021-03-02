82f85eb1253995471ef35bf8dc852cf2ad77857c
João Vitor <joaovq@gmail.com>
2021-03-02 16:35:38 -0300

    #refactor: melhorar exportaçao de pacotes

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,1 +1,17 @@
-export * from './utilities'
+import {
+  HttpRequest as IHttpRequest,
+} from '@/domain'
+import {
+  DateUtilitiesImpl,
+  ObjectUtilitiesImpl,
+  StringUtilitiesImpl,
+  FileUtilitiesImpl,
+  HttpRequestImpl,
+} from '@/data'
+
+export const dateUtilities = new DateUtilitiesImpl()
+export const objectUtilities = new ObjectUtilitiesImpl(dateUtilities)
+export const stringUtilities = new StringUtilitiesImpl()
+export const fileUtilities = new FileUtilitiesImpl()
+
+export const HttpRequest = (url: string): IHttpRequest => new HttpRequestImpl(url)
